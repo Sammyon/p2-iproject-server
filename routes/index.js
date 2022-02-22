d673c7f91c192ed47aff3448ebbd45e2703a9c7e
sammyon <samamandre.mandracon@gmail.com>
2022-02-22 15:13:43 +0700

    adding login/register

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const Controller = require("../controllers");
-const authenticator = require("../middlewares/authenticator");
-// const movies = require("./movies");
-// const users = require("./users");
-// const google = require("./google");
-// const customers = require("./customers");
+// const authenticator = require("../middlewares/authenticator");
+const user = require ("./user")
 
-// router.use("/users", users);
-// router.use("/google", google);
-// router.use("/customers", customers);
+router.use ('/users', user)
 
-router.use(authenticator);
-
-// router.get("/genres", Controller.listGenre)
-// router.get("/histories", Controller.listHistory)
-// router.use("/movies", movies);
+// router.use(authenticator);
 
 
-
+module.exports = router;
